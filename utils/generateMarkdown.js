60ae30264e306699c99779c498acf0de9ad2edf6
Vanessa Trinh <xtourmaline@gmail.com>
2023-11-27 22:02:07 -0500

    completed renderLicenseSection function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -28,7 +28,15 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
+    if (license === "None") {
+        return "";
+    } else if (license === "MIT") {
+        return "A short, permissive software license. Basically, you can do whatever you want as long as you include the original copyright and license notice in any copy of the software/source.  There are many variations of this license in use.";
+    } else if (license === "GNU GPLv3") {
+        return "You may copy, distribute and modify the software as long as you track changes/dates in source files. Any modifications to or software including (via compiler) GPL-licensed code must also be made available under the GPL along with build & install instructions.";
+    } else if (license === "MPL2.0") {
+        return "MPL is a copyleft license that is easy to comply with. You must make the source code for any of your changes available under MPL, but you can combine the MPL software with proprietary code, as long as you keep the MPL code in separate files. Version 2.0 is, by default, compatible with LGPL and GPL version 2 or greater. You can distribute binaries under a proprietary license, as long as you make the source available under MPL."
+    }
 }
 
 // TODO: Create a function to generate markdown for README
@@ -42,6 +50,8 @@ function generateMarkdown(data) {
 # ${data.title}
 
 ## Description
+${data.description}
+
 
 
 `;
